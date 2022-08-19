80870431ace9a75ffc81ce7efbdb1627f316044b
Atlas <potatoartie@gmail.com>
2022-08-19 23:03:26 +0900

    [init] project

---
+++ Assignment/Assignment/ViewController.swift
@@ -0,0 +1,19 @@
+//
+//  ViewController.swift
+//  Assignment
+//
+//  Created by Dongju on 2022/08/19.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
